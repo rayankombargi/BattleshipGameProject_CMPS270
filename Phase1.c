6fef6873bec842d0745a6dfdeae9592fcb8f9bbb
Carl Helayel <138108553+carlos10Xx@users.noreply.github.com>
2024-10-22 16:50:40 +0300

    added 50% chance to start

--- Phase1.c
+++ Phase1.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define N 10
 
-void Difficulty_Managment(char Difficlty[5]){
-
-
-
-}
-
-void Starting_Player(){
-
-
-    
+void Difficulty_Managment(char Difficlty[5])
+{
 }
 
 int main()
@@ -22,7 +15,7 @@ int main()
     char Difficulty[5];
     char Player_1_Name[50];
     char Player_2_Name[50];
-
+    int Random_Varible = (rand() % 2 + 1);
 
     for (int i = 0; i < N; i++)
     {
@@ -75,18 +68,23 @@ int main()
     }
 
     printf("Choose the difficulty (Easy/Hard): ");
-    scanf("%4s", &Difficulty);//In the easy mode, the grid will track both the hits and the misses,
-//                              whereas in the hard mode, the grid will only track hits
+    scanf("%4s", &Difficulty); // In the easy mode, the grid will track both the hits and the misses,
+    //                              whereas in the hard mode, the grid will only track hits
     printf("\n");
 
-
     printf("Player 1 please enter you name: ");
-    scanf("%49s",Player_1_Name );
+    scanf("%49s", Player_1_Name);
     printf("\n");
 
     printf("Player 2 please enter you name: ");
-    scanf("%49s",Player_2_Name );
-    printf("\n");    
+    scanf("%49s", Player_2_Name);
+    printf("\n");
+    if (Random_Varible == 1)
+    {
+        printf("Player 1 starts! \n");
+    }
+    else
+        printf("Player 2 starts!\n");
 
     return 0;
 }
