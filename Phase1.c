5102379fcf74ad681af56e5b08c2a946e204ab46
Carl <cke08@mail.aub.edu>
2024-10-24 17:16:20 +0300

    added the prototypes to the top of the main like rayyan did

--- Phase1.c
+++ Phase1.c
@@ -1,11 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
 
-#define N 10
-#define Max_Ships 4
-#define Max_Radar_Sweep 3
-#define Max_Smoke_Screen 4
+#define GRID_SIZE 10
+#define NUM_SHIPS 4
+
+#define EASY 0
+#define HARD 1
+
+#define MAX_RADAR_SWEEPS 3
+
+// Structure Definitions
+
+typedef struct {
+    char name[20];
+    int size;
+    int hits;
+    int sunk;
+    int coordinates[5][2];  // Maximum size is 5 (for Carrier)
+} Ship;
+
+typedef struct {
+    char display;    // What is shown on the grid ('~', '*', 'o')
+    int hasShip;     // 1 if there's a ship, 0 otherwise
+    int isHit;       // 1 if the cell has been targeted, 0 otherwise
+    int hasSmoke;    // 1 if the cell is under smoke screen, 0 otherwise
+} GridCell;
+
+typedef struct {
+    char name[50];
+    GridCell grid[GRID_SIZE][GRID_SIZE];
+    Ship ships[NUM_SHIPS];
+    int radarCount;
+    int smokeCount;
+    int artilleryAvailable;
+    int torpedoAvailable;
+    int shipsSunk;
+} Player;
+
+
+void initializeGrid(GridCell grid[GRID_SIZE][GRID_SIZE]);
+void displayGrid(GridCell grid[GRID_SIZE][GRID_SIZE], int trackingDifficulty);
+int getTrackingDifficulty();
+void initializePlayer(Player *player);
+void placeShips(Player *player);
+int validateAndPlaceShip(GridCell grid[GRID_SIZE][GRID_SIZE], Ship *ship, int row, int col, char *orientation);
+void performMove(Player *attacker, Player *defender, int trackingDifficulty);
+void showMoveOptions(Player *player);
+void performFire(Player *attacker, Player *defender, char *coord);
+void performRadar(Player *attacker, Player *defender, char *coord);
+void performSmoke(Player *player, char *coord);
+void performArtillery(Player *attacker, Player *defender, char *coord);
+void performTorpedo(Player *attacker, Player *defender, char *input);
+void updateShipStatus(Player *player, int row, int col);
+void unlockSpecialMoves(Player *player);
+int checkWinCondition(Player *player);
+void gameLoop(Player *player1, Player *player2, int trackingDifficulty);
+void clearScreen();
+void clearInputBuffer();
 void Guess_Maker(char Battle_Floor_General[N][N], char Difficulty[5]) // no need to do difficulty manager if we have here 
 {
 
@@ -44,83 +98,45 @@ void Guess_Maker(char Battle_Floor_General[N][N], char Difficulty[5]) // no need
     }
 }
 
-int main()
-{
-    char Battle_Floor_1[N][N];
-    char Battle_Floor_2[N][N];
-    char Difficulty[5];
-    char Player_1_Name[50];
-    char Player_2_Name[50];
-    int Random_Varible = (rand() % 2 + 1);
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            Battle_Floor_1[i][j] = '~';
-        }
+int main() {
+    srand(time(NULL));
+
+    Player player1, player2;
+    int trackingDifficulty;
+
+    printf("Welcome to Advanced Battleship!\n");
+
+    trackingDifficulty = getTrackingDifficulty();
+
+    initializePlayer(&player1);
+    clearScreen();
+
+    initializePlayer(&player2);
+    clearScreen();
+
+    // Each player places their ships
+    printf("\n%s, place your ships.\n", player1.name);
+    placeShips(&player1);
+    clearScreen();
+
+    printf("\n%s, place your ships.\n", player2.name);
+    placeShips(&player2);
+    clearScreen();
+
+    // Randomly choose the first player
+    Player *currentPlayer, *opponent;
+    if (rand() % 2 == 0) {
+        currentPlayer = &player1;
+        opponent = &player2;
+    } else {
+        currentPlayer = &player2;
+        opponent = &player1;
     }
+    printf("%s will go first.\n", currentPlayer->name);
 
-    printf("   ");
-    for (char Top_Letter_Indexing = 'A'; Top_Letter_Indexing < 'A' + N; Top_Letter_Indexing++)
-    {
-        printf("%c ", Top_Letter_Indexing);
-    }
-    printf("\n");
-
-    for (int i = 0; i < N; i++)
-    {
-        printf("%2d ", i + 1);
-        for (int j = 0; j < N; j++)
-        {
-            printf("%c ", Battle_Floor_1[i][j]);
-        }
-        printf("\n");
-    }
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            Battle_Floor_2[i][j] = '~';
-        }
-    }
-
-    printf("   ");
-    for (char Top_Letter_Indexing = 'A'; Top_Letter_Indexing < 'A' + N; Top_Letter_Indexing++)
-    {
-        printf("%c ", Top_Letter_Indexing);
-    }
-    printf("\n");
-
-    for (int i = 0; i < N; i++)
-    {
-        printf("%2d ", i + 1);
-        for (int j = 0; j < N; j++)
-        {
-            printf("%c ", Battle_Floor_2[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("Choose the difficulty (Easy/Hard): ");
-    scanf("%4s", &Difficulty); // In the easy mode, the grid will track both the hits and the misses,
-    //                              whereas in the hard mode, the grid will only track hits
-    printf("\n");
-
-    printf("Player 1 please enter you name: ");
-    scanf("%49s", Player_1_Name);
-    printf("\n");
-
-    printf("Player 2 please enter you name: ");
-    scanf("%49s", Player_2_Name);
-    printf("\n");
-    if (Random_Varible == 1)
-    {
-        printf("Player 1 starts! \n");
-    }
-    else
-        printf("Player 2 starts!\n");
+    // Start the game loop
+    gameLoop(currentPlayer, opponent, trackingDifficulty);
 
     return 0;
 }
