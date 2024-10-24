32c1f65bb3f5512f258f42dcd32b1ac11b79bf45
Carl <cke08@mail.aub.edu>
2024-10-24 17:28:00 +0300

    copied the main to test a method on my branch

--- Phase1.c
+++ Phase1.c
@@ -1,181 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 #include <time.h>
+#include <ctype.h>
 
-#define N 10
-#define Max_Ships 4
+#define GRID_SIZE 10
+#define NUM_SHIPS 4
+#define EASY 0
+#define HARD 1
+#define MAX_RADAR_SWEEPS 3
 
-void Clear_Board(char Battle_Floor[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            Battle_Floor[i][j] = '~'; // Resetting to water
-        }
-    }
-}
+typedef struct {
+    char name[20];
+    int size;
+    int hits;
+    int sunk;
+    int coordinates[5][2]; 
+} Ship;
 
-int Ship_Placment(char grid[N][N], int row, int column, int length, char orientation[12])
-{
-    if (orientation == "horizontal")
-    {
-        if (column + length > N)
-            return 0; // overflow condition
-        for (int j = column; j < column + length; j++)
-        {
-            if (grid[row][j] != '~') // checking if smth already exists
-                return 0;
-        }
-    }
-    else if (orientation == "vertical")
-    {
-        if (row + length > N)
-            return 0; // overflow condition
-        for (int i = row; i < row + length; i++)
-        {
-            if (grid[i][column] != '~') // checking if smth already exists
-                return 0;
-        }
-    }
-    return 1;
-}
+typedef struct {
+    char display;   
+    int hasShip;   
+    int isHit;      
+    int hasSmoke;   
+} GridCell;
 
-void Guess_Maker(char Battle_Floor_General[N][N], char Difficulty[5]) // no need to do difficulty manager if we have here
-{
+typedef struct {
+    char name[50];
+    GridCell grid[GRID_SIZE][GRID_SIZE];
+    Ship ships[NUM_SHIPS];
+    int radarCount;
+    int smokeCount;
+    int artilleryAvailable;
+    int torpedoAvailable;
+    int shipsSunk;
+} Player;
 
-    char col;
-    int row;
 
-    printf("Enter your guess: ");
-    scanf(" %c%d", &col, &row);
+void initializeGrid(GridCell grid[GRID_SIZE][GRID_SIZE]);
+void displayGrid(GridCell grid[GRID_SIZE][GRID_SIZE], int trackingDifficulty);
+int getTrackingDifficulty();
+void initializePlayer(Player *player);
+void placeShips(Player *player);
+int validateAndPlaceShip(GridCell grid[GRID_SIZE][GRID_SIZE], Ship *ship, int row, int col, char *orientation);
+void performMove(Player *attacker, Player *defender, int trackingDifficulty);
+void showMoveOptions(Player *player);
+void performFire(Player *attacker, Player *defender, char *coord);
+void performRadar(Player *attacker, Player *defender, char *coord);
+void performSmoke(Player *player, char *coord);
+void performArtillery(Player *attacker, Player *defender, char *coord);
+void performTorpedo(Player *attacker, Player *defender, char *input);
+void updateShipStatus(Player *player, int row, int col);
+void unlockSpecialMoves(Player *player);
+int checkWinCondition(Player *player);
+void gameLoop(Player *player1, Player *player2, int trackingDifficulty);
+void clearScreen();
+void clearInputBuffer();
 
-    int Column_Index = col - 'A'; // hon we just check the index using ascii code w the one under we do -1 to get index 0
-    int Row_Index = row - 1;
-
-    if (Row_Index >= 0 && Row_Index < N && Column_Index >= 0 && Column_Index < N)
-    {
-        if (Battle_Floor_General[Row_Index][Column_Index] == '~')
-        {
-            printf("LOSER you missed :) \n");
-            if (Difficulty == "easy")
-            {
-                Battle_Floor_General[Row_Index][Column_Index] = 'o';
-            }
-        }
-        else if (Battle_Floor_General[Row_Index][Column_Index] == '*')
-        {
-            printf("You already shot there\n");
-        }
-        else
-        {
-            printf("Bravo Darabta\n");
-            Battle_Floor_General[Row_Index][Column_Index] = '*';
-        }
-    }
-    else
-    {
-        printf("Choose a number from a-j and number 1-10\n");
-    }
-}
 
 
 int main() {
-    char Battle_Floor_1[N][N];
-    char Battle_Floor_2[N][N];
-    char Difficulty[5];
-    char Player_1_Name[50];
-    char Player_2_Name[50];
-    char Carrier_Location[6];
-    char Battle_Ship_Location[6];
-    char Destroyer_Location[6];
-    char Submarine_Location[6];
-    char Carrier_Orientation[13];     // size =5
-    char Battle_Ship_Orientation[13]; // size=4
-    char Destroyer_Orinetation[13];   // size= 3
-    char Submarine_Orientation[13];   // size=2
-    unsigned int seed = time(0);
-    srand(time(NULL)); // asked dr zalghout for this he said each time we run its a different time so we can use the probability changes to 50/50
-    int Random_Varible = (rand() % 2 + 1);
+    srand(time(NULL)); // to randomize dr zalgout said we can do it like dr zalghout said 
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            Battle_Floor_1[i][j] = '~';
-        }
+    Player player1, player2;
+    int trackingDifficulty;
+
+    trackingDifficulty = getTrackingDifficulty();
+
+    initializePlayer(&player1);
+    clearScreen();
+
+    initializePlayer(&player2);
+    clearScreen();
+
+    printf("\n%s, place your ships.\n", player1.name);
+    placeShips(&player1);
+    clearScreen();
+
+    printf("\n%s, place your ships.\n", player2.name);
+    placeShips(&player2);
+    clearScreen();
+
+    Player *currentPlayer, *opponent;
+    if (rand() % 2 == 0) {
+        currentPlayer = &player1;
+        opponent = &player2;
+    } else {
+        currentPlayer = &player2;
+        opponent = &player1;
     }
+    printf("%s will go first.\n", currentPlayer->name);
 
-    printf("   ");
-    for (char Top_Letter_Indexing = 'A'; Top_Letter_Indexing < 'A' + N; Top_Letter_Indexing++)
-    {
-        printf("%c ", Top_Letter_Indexing);
-    }
-    printf("\n");
-
-    for (int i = 0; i < N; i++)
-    {
-        printf("%2d ", i + 1);
-        for (int j = 0; j < N; j++)
-        {
-            printf("%c ", Battle_Floor_1[i][j]);
-        }
-        printf("\n");
-    }
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            Battle_Floor_2[i][j] = '~';
-        }
-    }
-
-    printf("   ");
-    for (char Top_Letter_Indexing = 'A'; Top_Letter_Indexing < 'A' + N; Top_Letter_Indexing++)
-    {
-        printf("%c ", Top_Letter_Indexing);
-    }
-    printf("\n");
-
-    for (int i = 0; i < N; i++)
-    {
-        printf("%2d ", i + 1);
-        for (int j = 0; j < N; j++)
-        {
-            printf("%c ", Battle_Floor_2[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("Choose the difficulty (Easy/Hard): ");
-    scanf("%4s", &Difficulty); // In the easy mode, the grid will track both the hits and the misses,
-    //                              whereas in the hard mode, the grid will only track hits
-    printf("\n");
-
-    printf("Player 1 please enter you name: ");
-    scanf("%49s", Player_1_Name);
-    printf("\n");
-
-    printf("Player 2 please enter you name: ");
-    scanf("%49s", Player_2_Name);
-    printf("\n");
-    if (Random_Varible == 1)
-    {
-        printf("Player 1 starts! \n");
-    }
-    else
-        printf("Player 2 starts!\n");
+    gameLoop(currentPlayer, opponent, trackingDifficulty);
 
     return 0;
+}
 
-// add player name and ask him for his move 
+void initializeGrid(GridCell grid[GRID_SIZE][GRID_SIZE]) {
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            grid[i][j].display = '~';
+            grid[i][j].hasShip = 0;
+            grid[i][j].isHit = 0;
+            grid[i][j].hasSmoke = 0;
+        }
+    }
+}
 
+void displayGrid(GridCell grid[GRID_SIZE][GRID_SIZE], int trackingDifficulty) {
+    printf("   A B C D E F G H I J\n");
+    for (int i = 0; i < GRID_SIZE; i++) {
+        printf("%2d", i + 1);
+        for (int j = 0; j < GRID_SIZE; j++) {
+            if (trackingDifficulty == HARD && grid[i][j].isHit == 0) {
+                printf(" ~");
+            } else {
+                printf(" %c", grid[i][j].display);
+            }
+        }
+        printf("\n");
+    }
+}
 
-    printf("enter the boat location and orientation for the Carrier (5 cells): ");
-    scanf("%s %s \n", Carrier_Location, Carrier_Orientation);
-    printf("enter the boat location and orientation for the Battleship (4 cells): ");
-    scanf("%s %s \n", Battle_Ship_Location, Battle_Ship_Orientation);
-    printf("enter the boat location and orientation for the Destroyer (3 cells): ");
-    scanf("%s %s \n", Destroyer_Location, Destroyer_Orinetation);
-    printf("enter the boat location and orientation for the Submarine (2 cells): ");
-    scanf("%s %s \n", Submarine_Location, Submarine_Orientation);
+int getTrackingDifficulty() {
+    int choice;
+    printf("Select tracking difficulty level:\n");
+    printf("1. Easy (tracks hits and misses)\n");
+    printf("2. Hard (tracks hits only)\n");
+    printf("Enter choice (1 or 2): ");
+    scanf("%d", &choice);
+    while (choice != 1 && choice != 2) {
+        printf("Invalid choice. Please enter 1 or 2: ");
+        scanf("%d", &choice);
+    }
+    clearInputBuffer();
+    return (choice == 1) ? EASY : HARD;
+}
+
+void initializePlayer(Player *player) { // got help from my friend (Fadel) on this due to some errors 
+    printf("Enter player name: ");
+    fgets(player->name, sizeof(player->name), stdin);
+    player->name[strcspn(player->name, "\n")] = '\0'; 
+    initializeGrid(player->grid);
+    player->radarCount = MAX_RADAR_SWEEPS;
+    player->smokeCount = 0;
+    player->artilleryAvailable = 0;
+    player->torpedoAvailable = 0;
+    player->shipsSunk = 0;
+
+    char *shipNames[NUM_SHIPS] = {"Carrier", "Battleship", "Destroyer", "Submarine"};
+    int shipSizes[NUM_SHIPS] = {5, 4, 3, 2};
+
+    for (int i = 0; i < NUM_SHIPS; i++) {
+        strncpy(player->ships[i].name, shipNames[i], sizeof(player->ships[i].name));
+        player->ships[i].size = shipSizes[i];
+        player->ships[i].hits = 0;
+        player->ships[i].sunk = 0;
+        memset(player->ships[i].coordinates, 0, sizeof(player->ships[i].coordinates));
+    }
 }
